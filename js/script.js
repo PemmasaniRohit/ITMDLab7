2da38c497921222598d8f73ef36d2a5b7af595bb
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 17:44:25 -0500

    Update script.js
    Spacing change

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
+    
   function textalert(){
-    alert("Rohit Pemmasani: " + "Hello is this working?");
+    alert("Rohit Pemmasani: ");
   }
-  
   redAlertButton.addEventListener('click', textalert);
-
-  
-
-
-
-
-
-
   }
 window.addEventListener('load', init);

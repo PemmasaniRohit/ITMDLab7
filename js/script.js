415d1f8bafde082572cabd8433160a4b5e09d755
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 18:11:00 -0500

    Update script.js
    Variable created and added to the string and h3 value

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
   function textalert(){
-    //var userSelection = document.getElementById('entryinput');
-    //document.getElementById('textoutput').innerHTML = userSelection.value;
-    alert("Rohit Pemmasani: ");
+    var userSelection = document.getElementById('entryinput');
+    var A =  userSelection.value
+    document.getElementById('textoutput').innerHTML =A;
+    alert("Rohit Pemmasani: "+A);
   }
   redAlertButton.addEventListener('click', textalert);
   }

88fac238d00e19ee726603799375defa18698e66
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 17:31:30 -0500

    Update script.js
    Alert added as a check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
+  alert("Rohit Pemmasani: " + "Hello is this working?");
   
  
 

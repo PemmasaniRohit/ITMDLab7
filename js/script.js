7d805a41b8d3b31dba261b889cd3e98a64b7c8ae
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 17:57:31 -0500

    Update script.js
    Inner HTML added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
-    
+  alert("Alert is working");
   function textalert(){
     var userSelection = document.getElementById('entryinput');
-    document.getElementById('textoutput') = userSelection.value;
+    document.getElementById('textoutput').innerHTML = userSelection.value;
     alert("Rohit Pemmasani: "+userSelection.value);
   }
   redAlertButton.addEventListener('click', textalert);

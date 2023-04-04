6c38c679e3abedfff95ae3e872a8e3213a8c707c
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 18:04:04 -0500

    Update script.js
    Commented code to see function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
   function textalert(){
-    var userSelection = document.getElementById('entryinput');
-    document.getElementById('textoutput').innerHTML = userSelection.value;
-    alert("Rohit Pemmasani: " + userSelection.value);
+    //var userSelection = document.getElementById('entryinput');
+    //document.getElementById('textoutput').innerHTML = userSelection.value;
+    alert("Rohit Pemmasani: ");
   }
   redAlertButton.addEventListener('click', textalert);
   }

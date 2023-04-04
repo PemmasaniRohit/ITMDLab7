c481ee7294d353591e7093ab2fb0789bb6479a1a
PemmasaniRohit <90728740+PemmasaniRohit@users.noreply.github.com>
2023-04-04 17:34:03 -0500

    Update script.js
    Added a listener

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
   var redAlertButton = document.getElementById('entrybutton');
-  alert("Rohit Pemmasani: " + "Hello is this working?");
+  a = alert("Rohit Pemmasani: " + "Hello is this working?");
+  redAlertButton.addEventListener('click', a);
+
   
- 
 
 
 
